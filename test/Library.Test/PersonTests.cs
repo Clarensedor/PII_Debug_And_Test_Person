2bd310c1b02c6371d05a34519ff91a503b2cc761
Clarensedor <101676526+Clarensedor@users.noreply.github.com>
2022-04-24 23:22:58 -0300

    tarea
    Se agregala tarea

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -12,10 +12,37 @@ namespace Tests
             // Insertá tu código de inicialización aquí
         }
 
+
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void WithValidName() // Cambiá el nombre para indicar qué estás probando
         {
-            // Insertá tu código  de pruebaaquí
+            string validName = "Anthony";
+            Person uno = new Person(validName, "");
+            Assert.AreEqual(validName, uno.Name);
+        }
+
+        [Test]
+        public void WithInvalidName()
+        {
+            string invalidName = "";
+            Person uno = new Person(invalidName, "");
+            Assert.AreEqual(null, uno.Name);
+        }
+
+        [Test]
+        public void WithValidId() 
+        {
+            const bool validId = true;
+            Person uno = new Person("", "1.234.890-4");
+            Assert.AreEqual(validId, string.IsNullOrEmpty(uno.ID));
+        }
+
+        [Test]
+        public void WithInvalidId()
+        {
+            string invalidId = "1111111111111111111111111111";
+            Person uno = new Person("", invalidId);
+            Assert.AreEqual(null, uno.ID);
         }
     }
 }
